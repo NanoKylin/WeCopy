c739b311194f5d04ec669c6869964dd3d6d5e50c
Hanbings <3219065882@qq.com>
2020-12-25 22:21:59 +0800

    :sparkles: Feat: Main Page

--- js/wecopy.js
+++ js/wecopy.js
@@ -23,3 +23,7 @@ function login() {
         alert('当前浏览器不支持WebSocket 请更换浏览器');
     }
 }
+
+function loadDefaultCopy() {
+    document.write("<div class=\"row\"><div class=\"col s12 m6\"><div class=\"card blue-grey darken-1\"><div class=\"card-content white-text\"><span class=\"card-title\">卡片标题</span><p>我是一个很简单的卡片。我很擅长于包含少量的信息。我很方便，因为我只需要一个小标记就可以有效地使用。</p></div><div class=\"card-action\"><a href=\"#\">这是一个链接</a><a href=\"#\">这是一个链接</a></div></div></div></div>")
+}
