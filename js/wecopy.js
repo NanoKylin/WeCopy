75004c1debd2533d3df113754c9888dfe6133d9e
Hanbings <3219065882@qq.com>
2020-12-27 09:24:13 +0800

    :fire:  Feat: Remove Code

--- js/wecopy.js
+++ js/wecopy.js
@@ -50,51 +50,10 @@ function getContext() {
 }
 
 function setUsernameFromLocalStorage() {
-    alert(getClipboard());
     this.username = localStorage.getItem("username");
     this.server = localStorage.getItem("server");
 }
 
 function checkCloudCopyThread() {
     websocket.send("GCSC02{\"username\":" + username + ",\"context\":\"null text\"}EE");
-}
-
-/**
- * 读取剪切板内容
- */
-function getClipboard() {
-    if (window.clipboardData) {
-        return (window.clipboardData.getData('Text'));
-    } else if (window.netscape) {
-        netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
-        var clip = Components.classes['@mozilla.org/widget/clipboard;1'].createInstance(Components.interfaces.nsIClipboard);
-        if (!clip) return;
-        var trans = Components.classes['@mozilla.org/widget/transferable;1'].createInstance(Components.interfaces.nsITransferable);
-        if (!trans) return;
-        trans.addDataFlavor('text/unicode');
-        clip.getData(trans, clip.kGlobalClipboard);
-        var str = new Object();
-        var len = new Object();
-        try {
-            trans.getTransferData('text/unicode', str, len);
-        } catch (error) {
-            return null;
-        }
-        if (str) {
-            if (Components.interfaces.nsISupportsWString) str = str.value.QueryInterface(Components.interfaces.nsISupportsWString);
-            else if (Components.interfaces.nsISupportsString) str = str.value.QueryInterface(Components.interfaces.nsISupportsString);
-            else str = null;
-        }
-        if (str) {
-            return (str.data.substring(0, len.value / 2));
-        }
-    }
-    return null;
-}
-
-function readClipboardData() {
-    var str = getClipboard();
-    var len = str.split("\n"); //获取行数
-
-    document.getElementById("txtContent").value = str;
 }
